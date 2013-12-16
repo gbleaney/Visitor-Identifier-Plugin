3eba016e14ebb90c736317c115bf3b723ae0cf25
Graham Bleaney <gbleaney@gmail.com>
2013-12-15 21:43:33 -0500

    Added visitor ip retrieval to every page and added creation of database on plugin init

--- Visitor-Identifier-Plugin.php
+++ Visitor-Identifier-Plugin.php
@@ -24,9 +24,56 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+
+//SETUP
+
+//Global vars
+global $visitor_identifier_db_version;
+$visitor_identifier_db_version = "0.1";
+//Create DB on first use
+register_activation_hook( __FILE__, 'setup_visitor_identifier_database' );
+//Add tracking code to every page
 add_action( 'admin_menu', 'visitor_identifier_plugin_menu' );
+//Crate admin page
+add_action( 'wp_after_admin_bar_render', 'add_tracking_to_page');
 
 
+//One time installation actions
+function setup_visitor_identifier_database(){
+    global $wpdb;
+    global $visitor_identifier_db_version;
+
+    $table_name = $wpdb->prefix . "visitoridentifierlogs"; 
+
+    $sql = "CREATE TABLE ".$table_name." (
+      ip BINARY(16) NOT NULL,
+      time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+      orgname tinytext,
+      fullxml text NOT NULL,
+      UNIQUE KEY ip (ip)
+    );";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    add_option( "visitor_identifier_db_version", $visitor_identifier_db_version );
+}
+
+
+//On page tracking code
+function add_tracking_to_page() {
+    $userIp = get_user_ip();
+
+    $xml = wp_remote_get( 'http://www.whoisxmlapi.com/whoisserver/WhoisService?domainName='.$userIp );
+
+    $simpleXml = simplexml_load_string($xml["body"]);
+    var_dump($simpleXml);
+    echo "MADE IT!";
+    echo $simpleXml["registrant"]["orgainization"];
+}
+
+
+//Page creation functions
 function visitor_identifier_plugin_menu() {
 	add_menu_page( 'Visitor Identifier', 'Visitor Identifier', 'manage_options', 'Visitor-Identifier-Plugin', 'visitor_identifier_page'. "", 100 );
 }
@@ -35,15 +82,15 @@ function visitor_identifier_page() {
 	if ( !current_user_can( 'manage_options' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
-	$userIp = get_user_ip();
+    global $wpdb;
 
-	$xml = wp_remote_get( 'http://www.whoisxmlapi.com/whoisserver/WhoisService?domainName='.$userIp );
-
-	$simpleXml = simplexml_load_string($xml["body"]);
-	var_dump($simpleXml);
-	echo $simpleXml["registrant"]["orgainization"];
+    $table_name = $wpdb->prefix . "visitoridentifierlogs";
+	$visitorinfo = $wpdb->get_results( "SELECT * FROM ".$table_name );
+	var_dump($visitorinfo);
 }
 
+
+//Utility functions
 function get_user_ip() {
     $ip = $_SERVER['REMOTE_ADDR'];
     return $ip;
